2e7292be0c49c41560037029d93ce8ead0137371
Kacode123 <98067570+Kacode123@users.noreply.github.com>
2022-02-17 12:26:29 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,5 @@
+def on_forever():
+    basic.show_string("I am")
+    basic.show_string("Michael!")
+    basic.show_string("Hello!")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function on_forever() {
+    basic.showString("I am")
+    basic.showString("Michael!")
+    basic.showString("Hello!")
+})
